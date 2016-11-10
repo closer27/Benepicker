fe36e49ea9747bbc7e345ceca78f0fbb3b01ba4a
Seungwon Kim <closer527@gmail.com>
2016-11-10 19:09:57 +0900

    add gitignore

---
+++ Benepicker/Benepicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Benepicker
+//
+//  Created by 1002719 on 2016. 11. 10..
+//  Copyright © 2016년 closer27. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
