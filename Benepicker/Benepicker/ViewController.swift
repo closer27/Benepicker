bdddbcfb0f08c2678d2f2b3f1d662d9dd43daf28
Seungwon Kim <closer527@gmail.com>
2016-11-13 23:28:20 +0900

    add Regex class, validate date pattern

--- Benepicker/Benepicker/ViewController.swift
+++ Benepicker/Benepicker/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let receiption: String = "[Web발신]\n" +
+        "KB국민카드 4*6*\n" +
+        "김*원님\n" +
+        "11/03 09:29\n" +
+        "3,300원\n" +
+        "스타벅스커피코\n" +
+        "누적 490,040원\n"
+        
+        let items: [String] = receiption.characters.split{$0 == "\n"}.map(String.init)
+        let datePattern: String = "\\d{2}/\\d{2} \\d{2}:\\d{2}"
+        for item in items {
+            if item =~ datePattern {
+                print("\(item) is date")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
