640008a141344d5b9117efa685e46540587db8ab
Seungwon Kim <closer527@gmail.com>
2016-11-19 18:59:23 +0900

    add a simple test case for verifying receipts

--- Benepicker/BenepickerTests/BenepickerTests.swift
+++ Benepicker/BenepickerTests/BenepickerTests.swift
@@ -10,7 +10,6 @@ import XCTest
 @testable import Benepicker
 
 class BenepickerTests: XCTestCase {
-    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -21,9 +20,18 @@ class BenepickerTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testReceiptCount() {
+        let aMessage = "[Web발신]\n" +
+        "KB국민카드 4*6*\n" +
+        "김*원님\n" +
+        "11/02 23:32\n" +
+        "1,700원\n" +
+        "세븐일레븐 안양\n" +
+        "누적 486,740원\n"
+        
+        let receiptParser = ReceiptParser.init()
+        let receipts = receiptParser.receiptsFromString(aMessage)
+        XCTAssertEqual(receipts.count, 1, "receipt count is 1")
     }
     
     func testPerformanceExample() {
