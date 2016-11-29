64f506b8ae51d5ef18768807bea3b280cb1c1b63
Seungwon Kim <closer527@gmail.com>
2016-11-29 20:04:07 +0900

    add UITableView to show receipts, create a custom tableViewCell basically

---
+++ Benepicker/Benepicker/ReceiptTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ReceiptTableViewCell.swift
+//  Benepicker
+//
+//  Created by closer27 on 2016. 11. 29..
+//  Copyright © 2016년 closer27. All rights reserved.
+//
+
+import UIKit
+
+class ReceiptTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var spendLabel: UILabel!
+    @IBOutlet weak var usedPlaceLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+}
--- Benepicker/Benepicker/ViewController.swift
+++ Benepicker/Benepicker/ViewController.swift
@@ -8,50 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    let receiptParser:ReceiptParser = ReceiptParser.init()
-    var receipts:[Receipt] = []
-    
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet var tableView: UITableView!
+
+    let receiptParser: ReceiptParser = ReceiptParser.init()
+    var receipts: [Receipt] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        let receiptRawMessages: String = "[Web발신]\n" +
-        "KB국민카드 4*6*\n" +
-        "김*원님\n" +
-        "11/03 09:29\n" +
-        "3,300원\n" +
-        "스타벅스커피코\n" +
-        "누적 490,040원\n" +
-        "[Web발신]\n" +
-        "KB국민카드 4*6*\n" +
-        "김*원님\n" +
-        "11/02 23:32\n" +
-        "1,700원\n" +
-        "세븐일레븐 안양\n" +
-        "누적 486,740원\n"
-//        "2.19(US$)\n"
-        
-        let receiptParser:ReceiptParser = ReceiptParser.init()
-        let receipts:[Receipt] = receiptParser.receiptsFromString(receiptRawMessages)
-        for receipt in receipts {
-            print(receipt.description)
-        }
-        
+
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     @IBAction func pasteMessages(_ sender: Any) {
-        if UIPasteboard.general.hasStrings {
-            if let receiptMessages = UIPasteboard.general.strings {
-                receipts = receiptParser.receiptsFromString(receiptMessages[0])
-                print(receipts[0])
-            }
+        if UIPasteboard.general.hasStrings, let receiptMessages = UIPasteboard.general.strings {
+            receipts = receiptParser.receiptsFromString(receiptMessages[0])
+            tableView.reloadData()
         }
     }
+
+    /* TableView DataSource */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return receipts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: ReceiptTableViewCell = tableView.dequeueReusableCell(withIdentifier: "receiptCellIdentifier",
+                for: indexPath) as! ReceiptTableViewCell
+        let row = indexPath.row;
+
+        let receipt: Receipt = receipts[row]
+        cell.spendLabel.text = receipt.spend
+        cell.dateLabel.text = receipt.date
+        cell.usedPlaceLabel.text = receipt.usedPlace
+
+        return cell
+    }
+
+    /* TableView Delegate */
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 58
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+
 }
 
